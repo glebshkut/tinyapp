72e197546eece99c3d3a0f61ec1c4fb5d094cd74
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-24 15:41:15 -0800

    added new longURL feature and randomString assign

--- express_server.js
+++ express_server.js
@@ -1,5 +1,18 @@
+function generateRandomString() {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; 
 
 app.set("view engine", "ejs");
@@ -18,14 +31,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-})
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
