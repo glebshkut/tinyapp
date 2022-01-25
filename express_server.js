80c869baeb0b7a6846ce8d01318d8ddd6e6bab61
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-24 17:55:14 -0800

    added /u/:shortURL and redirection to shortURL page after its creation

--- express_server.js
+++ express_server.js
@@ -40,14 +40,20 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.post("/urls", (req, res) => {
-  urlDatabase[generateRandomString()] = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
   console.log(req.body);
-  res.send("Ok");
+  res.redirect(`/urls/${shortURL}`);
 })
 
 app.listen(PORT, () => {
